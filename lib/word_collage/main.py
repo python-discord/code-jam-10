d2dd42984f188096de7dc88c724f4387f222466e
leoybkim <leoybkim@gmail.com>
2023-09-02 01:12:28 -0700

    Refactor ascii art generator script

--- lib/word_collage/main.py
+++ lib/word_collage/main.py
@@ -1,9 +1,10 @@
 import argparse
+
 import numpy as np
 from PIL import Image, ImageDraw
 
 
-def avg_greyscale(img):
+def avg_greyscale(img: Image):
     """
     Calculates average greyscale or luminance value of an input image
 
@@ -15,7 +16,7 @@ def avg_greyscale(img):
     return np.average(np_img.reshape(w * h))
 
 
-def img_to_ascii(img, cols, scale, dens):
+def img_to_ascii(img: Image, cols, scale, dens):
     """
     Given Image and dims (rows, cols) returns an m*n list of Images
 
@@ -29,14 +30,15 @@ def img_to_ascii(img, cols, scale, dens):
     greyscale_strs = [
         "@%#*+=-:. ",  # 10 characters,
         "@%W*hdwOLUzurt|1i}?+>,' ",  # 24 characters,
-        "@B$%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1lIi!{}[]?-_+~<>;:,^`'. "  # 70 characters,
+        "@B$%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1lIi!{}[]?-_+~<>;:,^`'. ",  # 70 characters,
     ]
 
-    gs_img = Image.open(img).convert("L")  # Convert to grayscale (L stands for luminance)
+    # Convert to grayscale (L stands for luminance)
+    gs_img = Image.open(img).convert("L")
     gs_img_w, gs_img_h = gs_img.size[0], gs_img.size[1]
 
-    w = gs_img_w / cols  # Output width
-    h = w / scale  # Output height
+    w = gs_img_w / cols  # tile width
+    h = w / scale  # tile height
     rows = int(gs_img_h / h)  # Number of rows of text
 
     print(f"Original image size: {gs_img_w} x {gs_img_h}")
@@ -58,28 +60,30 @@ def img_to_ascii(img, cols, scale, dens):
             x1 = int(c * w)
             x2 = gs_img_w if c == cols - 1 else int((c + 1) * w)
 
-            avg = int(avg_greyscale(gs_img.crop((x1, y1, x2, y2))))  # Find average luminance
+            # Find average luminance of the cropped tile
+            avg = int(avg_greyscale(gs_img.crop((x1, y1, x2, y2))))
 
             # Find corresponding ascii character by greyscale value and append to the row
-            ascii_[r] += greyscale_strs[dens][int((avg * (len(greyscale_strs[dens]) - 1)) / 255)]
+            i = int((avg * (len(greyscale_strs[dens]) - 1)) / 255)
+            ascii_[r] += greyscale_strs[dens][i]
 
     return ascii_
 
 
-def ascii_to_img():
+def ascii_to_img(ascii_text_file, output_dest):
     """
     Creates image file from ascii text file
 
     :return: generated ascii image
     """
-    with open("result.txt", "r") as file:
+    with open(ascii_text_file, "r") as file:
         ascii_text = file.read()
 
     img_w, img_h = (750, 750)
-    im = Image.new("RGBA", (img_w, img_h), "white")
+    img = Image.new("RGBA", (img_w, img_h), "white")
 
     # Draw text to image
-    draw = ImageDraw.Draw(im)
+    draw = ImageDraw.Draw(img)
     bbox = draw.textbbox(xy=(0, 0), text=ascii_text)
     left, top, right, bottom = bbox
     w = right - left
@@ -88,19 +92,63 @@ def ascii_to_img():
     # Center the drawing
     draw.text(((img_w - w) / 2, (img_h - h) / 2), ascii_text, fill="black")
 
-    im.save("result.png", "png")
+    img.save(output_dest, "png")
+
+
+def seed_secret(secret):
+    pass
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--img", dest="img", required=True)
-    parser.add_argument("--scale", dest="scale", required=False, default=0.5)
-    parser.add_argument("--cols", dest="cols", required=False, default=100)
-    parser.add_argument("--dens", dest="dens", required=False, default=2)
+    parser = argparse.ArgumentParser(
+        description="Converts input image into ascii art and saves the resulting text back into image (.png)"
+    )
+    parser.add_argument("--input", dest="input", required=True, help="Input image")
+    parser.add_argument(
+        "--secret",
+        dest="secret",
+        required=True,
+        help="Secret phrase to hide in the ascii art",
+    )
+    parser.add_argument(
+        "--ascii-file",
+        dest="ascii_file",
+        required=False,
+        default="ascii.txt",
+        help="Output location for generated ascii text file",
+    )
+    parser.add_argument(
+        "--output",
+        dest="output",
+        required=False,
+        default="result.png",
+        help="Output location for generated image file",
+    )
+    parser.add_argument(
+        "--scale",
+        dest="scale",
+        required=False,
+        default=0.5,
+        help="Aspect ratio used to calculate tile size",
+    )
+    parser.add_argument(
+        "--cols",
+        dest="cols",
+        required=False,
+        default=100,
+        help="Tile chunk size (width) in number pixels",
+    )
+    parser.add_argument(
+        "--density",
+        dest="dens",
+        required=False,
+        default=2,
+        help="Resolution of greyscale (0: Low, 1: Medium, 2: High (Default))",
+    )
     args = parser.parse_args()
 
-    with open("result.txt", "w") as f:
-        for row in img_to_ascii(args.img, int(args.cols), float(args.scale), int(args.dens)):
-            f.write(row + "\n")
-
-    ascii_to_img()
+    with open(args.ascii_file, "w") as f:
+        for r in img_to_ascii(args.input, int(args.cols), float(args.scale), int(args.dens)):
+            f.write(r + "\n")
+    seed_secret(args.secret)
+    ascii_to_img(args.ascii_file, args.output)
