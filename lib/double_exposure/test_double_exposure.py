30004d905d38ff45427352bfcf34da19ddf39aca
dfebs <daniel.febles97@gmail.com>
2023-09-04 13:31:01 -0400

    Added a test case for double exposure plus some extra assertions

--- lib/double_exposure/test_double_exposure.py
+++ lib/double_exposure/test_double_exposure.py
@@ -8,15 +8,15 @@ from .double_exposure import double_exposure
 class DoubleExposureTestCase(unittest.TestCase):
     """Tests For double_exposure.py"""
 
-    def test_size(self) -> None:
+    def test_alpha_half(self) -> None:
         """
-        Test that the double_exposure function preserves the size of the input images.
+        Test that blending with default alpha=0.5 results in a blend of two images and preserves image size.
 
         :return: None
         """
         # Load two example images
-        image1 = Image.new("RGB", (200, 200))
-        image2 = Image.new("RGB", (200, 200))
+        image1 = Image.new("RGB", (200, 200), (0, 255, 0))
+        image2 = Image.new("RGB", (200, 200), (255, 0, 0))
 
         # Call the double_exposure function
         result_image = double_exposure(image1, image2)
@@ -24,9 +24,13 @@ class DoubleExposureTestCase(unittest.TestCase):
         # Check if the size of the result image matches the input images
         self.assertEqual(result_image.size, (200, 200))
 
+        # Check if the result image matches a blend of the two images
+        expected_image = Image.new("RGB", (200, 200), (127, 127, 0))
+        self.assertEqual(result_image, expected_image)
+
     def test_alpha_zero(self) -> None:
         """
-        Test that blending with alpha=0.0 results in the first image unchanged.
+        Test that blending with alpha=0.0 results in the first image unchanged and preserves image size.
 
         :return: None
         """
@@ -37,12 +41,15 @@ class DoubleExposureTestCase(unittest.TestCase):
         # Blend the images with alpha = 0.0 (result should be the first image)
         result_image = double_exposure(image1, image2, alpha=0.0)
 
+        # Check if the size of the result image matches the input images
+        self.assertEqual(result_image.size, (200, 200))
+
         # Check if the result image matches the first image
         self.assertEqual(result_image, image1)
 
     def test_alpha_one(self) -> None:
         """
-        Test that blending with alpha=1.0 results in the second image unchanged.
+        Test that blending with alpha=1.0 results in the second image unchanged and preserves image size.
 
         :return: None
         """
@@ -52,6 +59,9 @@ class DoubleExposureTestCase(unittest.TestCase):
 
         # Blend the images with alpha = 1.0 (result should be the second image)
         result_image = double_exposure(image1, image2, alpha=1.0)
+
+        # Check if the size of the result image matches the input images
+        self.assertEqual(result_image.size, (200, 200))
 
         # Check if the result image matches the second image
         self.assertEqual(result_image, image2)
