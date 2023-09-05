f0fd8228d8ee9a21a1980f8480ecc66c9fe20f18
amoooooo <83256424+amooo-ooo@users.noreply.github.com>
2023-09-06 07:43:45 +1200

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,6 @@ from PyQt6.QtWidgets import QApplication
 from level import Level
 from window import Window
 
-
 def main() -> None:
     """Main function"""
     app = QApplication(sys.argv)
@@ -14,13 +13,8 @@ def main() -> None:
     current_level = Level(1)
     window = Window(current_level)
 
-    # Apply CSS (QSS) Styling
-    with open(Path('styles/styles.css')) as qss:
-        app.setStyleSheet(qss.read())
-
     window.show()
     sys.exit(app.exec())
-
 
 if __name__ == '__main__':
     main()
