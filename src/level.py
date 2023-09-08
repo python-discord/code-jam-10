e37fe90e846da03bc02e79c4636b84c9e40b55d2
leoybkim <leoybkim@gmail.com>
2023-09-07 23:28:03 -0700

    Get icon for level 3

--- src/level.py
+++ src/level.py
@@ -140,7 +140,7 @@ class Level:
             ],
             [
                 (
-                    Path(icons_dir_path, "button_sample.png"),
+                    Path(icons_dir_path, "magnifying_glass.png"),
                     ControlPanel(
                         "Hidden in ASCII",
                         []
