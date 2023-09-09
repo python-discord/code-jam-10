b3cd207ad1ef2fa17cc0ff889358f216f09061a0
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-09-09 15:26:32 -0700

    Remove old files

--- src/decode.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from PIL import Image
-
-from main import decode
-
-if __name__ == "__main__":
-    decoded = decode(Image.open(sys.argv[1])).decode("utf-8")
-    print(decoded)
--- src/encode.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-from main import encode
-
-if __name__ == "__main__":
-    with open(sys.argv[1], "rb") as file:
-        encoded = encode(file.read())
-        encoded.save(sys.argv[1] + ".png")
