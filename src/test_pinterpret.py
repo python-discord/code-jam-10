70dafba857eb831358d3044d74d2743f0f51c959
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-09-08 20:09:21 -0700

    Update tests

--- src/test_pinterpret.py
+++ src/test_pinterpret.py
@@ -1,32 +1,30 @@
 import random
 import string
+from collections import deque
+from io import StringIO
 
-import numpy as np
+import pytest
 
-from pinterpret import CodelChooserValues, DirectionPointerValues, PietRuntime
-
-global piet_runtime
-
-
-def test_runtime_create():
-    global piet_runtime
-    piet_runtime = PietRuntime("69 test")
+from pinterpret import CodelChooserDirection, PietRuntime, PointerDirection
 
 
-def test_push_instruction():
-    global piet_runtime
+@pytest.fixture(name="piet_runtime")
+def fixture_piet_runtime():
+    return PietRuntime(StringIO("69 test"))
+
+
+def test_push_instruction(piet_runtime: PietRuntime):
     piet_runtime.p_push(1)
     assert piet_runtime.stack[0] == 1
 
 
-def test_pop_instruction():
-    global piet_runtime
+def test_pop_instruction(piet_runtime: PietRuntime):
+    piet_runtime.p_push(1)
     piet_runtime.p_pop()
-    assert piet_runtime.stack._stack.size == 0
+    assert len(piet_runtime.stack._stack) == 0
 
 
-def test_add_instruction():
-    global piet_runtime
+def test_add_instruction(piet_runtime: PietRuntime):
     num1, num2 = random.choice(range(1, 100)), random.choice(range(1, 100))
     piet_runtime.p_push(num1)
     piet_runtime.p_push(num2)
@@ -35,8 +33,7 @@ def test_add_instruction():
     piet_runtime.p_pop()
 
 
-def test_subtract_instruction():
-    global piet_runtime
+def test_subtract_instruction(piet_runtime: PietRuntime):
     num1, num2 = random.choice(range(1, 100)), random.choice(range(1, 100))
     piet_runtime.p_push(num1)
     piet_runtime.p_push(num2)
@@ -45,8 +42,7 @@ def test_subtract_instruction():
     piet_runtime.p_pop()
 
 
-def test_multiply_instruction():
-    global piet_runtime
+def test_multiply_instruction(piet_runtime: PietRuntime):
     num1, num2 = random.choice(range(1, 100)), random.choice(range(1, 100))
     piet_runtime.p_push(num1)
     piet_runtime.p_push(num2)
@@ -55,8 +51,7 @@ def test_multiply_instruction():
     piet_runtime.p_pop()
 
 
-def test_division_instruction():
-    global piet_runtime
+def test_division_instruction(piet_runtime: PietRuntime):
     num1, num2 = random.choice(range(1, 100)), random.choice(range(1, 100))
     piet_runtime.p_push(num1)
     piet_runtime.p_push(num2)
@@ -65,8 +60,7 @@ def test_division_instruction():
     piet_runtime.p_pop()
 
 
-def test_modulo_instruction():
-    global piet_runtime
+def test_modulo_instruction(piet_runtime: PietRuntime):
     num1, num2 = random.choice(range(1, 100)), random.choice(range(1, 100))
     piet_runtime.p_push(num1)
     piet_runtime.p_push(num2)
@@ -75,8 +69,7 @@ def test_modulo_instruction():
     piet_runtime.p_pop()
 
 
-def test_not_instruction():
-    global piet_runtime
+def test_not_instruction(piet_runtime: PietRuntime):
     choice = random.choice([1, 0])
     piet_runtime.p_push(choice)
     piet_runtime.p_not()
@@ -84,8 +77,7 @@ def test_not_instruction():
     piet_runtime.p_pop()
 
 
-def test_greater_instruction():
-    global piet_runtime
+def test_greater_instruction(piet_runtime: PietRuntime):
     num1, num2 = random.choice(range(1, 100)), random.choice(range(1, 100))
     piet_runtime.p_push(num1)
     piet_runtime.p_push(num2)
@@ -94,70 +86,59 @@ def test_greater_instruction():
     piet_runtime.p_pop()
 
 
-def test_pointer_instruction():
-    global piet_runtime
+def test_pointer_instruction(piet_runtime: PietRuntime):
     piet_runtime.p_push(2)
     piet_runtime.p_pointer()
-    assert piet_runtime.pointer.position == DirectionPointerValues.LEFT
+    assert piet_runtime.pointer.direction == PointerDirection.LEFT
 
 
-def test_switch_instruction():
-    global piet_runtime
+def test_switch_instruction(piet_runtime: PietRuntime):
     piet_runtime.p_push(1)
     piet_runtime.p_switch()
-    assert piet_runtime.codel_chooser.position == CodelChooserValues.RIGHT
+    assert piet_runtime.codel_chooser.direction == CodelChooserDirection.RIGHT
 
 
-def test_duplicate_instruction():
-    global piet_runtime
+def test_duplicate_instruction(piet_runtime: PietRuntime):
     num = random.choice(range(1, 100))
     piet_runtime.p_push(num)
     piet_runtime.p_duplicate()
-    assert np.array_equal(piet_runtime.stack._stack, np.array([num, num], dtype=np.int64))
-    _ = piet_runtime.stack.popx()
+    assert piet_runtime.stack.pop_multiple() == [num, num]
 
 
-def test_roll_instruction():
-    global piet_runtime
-    base_array = np.array(list(random.randbytes(random.choice(range(1, 10)))))
-    edge_array = np.array(list(random.randbytes(random.choice(range(1, 10)))))
+def test_roll_instruction(piet_runtime: PietRuntime):
+    base_stack = deque(random.randbytes(random.choice(range(1, 10))))
+    edge_stack = deque(random.randbytes(random.choice(range(1, 10))))
     roll_count = random.choice(range(1, 10))
-    piet_runtime.stack._stack = np.copy(
-        np.append(np.append(base_array, edge_array), np.array([edge_array.size, roll_count], dtype=np.int64))
-    )
-    rolled_array = np.roll(edge_array, roll_count)
+    piet_runtime.stack._stack.extend(base_stack)
+    piet_runtime.stack._stack.extend(edge_stack)
+    piet_runtime.stack._stack.extend((len(edge_stack), roll_count))
+    edge_stack.rotate(roll_count)
     piet_runtime.p_roll()
-    assert np.array_equal(piet_runtime.stack._stack, np.append(base_array, rolled_array))
-    piet_runtime.stack._stack = np.array([], dtype=np.int64)
+    base_stack.extend(edge_stack)
+    assert piet_runtime.stack._stack == base_stack
 
 
-def test_input_num_instruction():
-    global piet_runtime
+def test_input_num_instruction(piet_runtime: PietRuntime):
     piet_runtime.p_input_num()
     assert (piet_runtime.input_buffer == " test") and (piet_runtime.stack[0] == 69)
-    piet_runtime.p_pop()
 
 
-def test_input_char_instruction():
-    global piet_runtime
+def test_input_char_instruction(piet_runtime: PietRuntime):
     piet_runtime.p_input_char()
     piet_runtime.p_input_char()
     piet_runtime.p_input_char()
     piet_runtime.p_input_char()
     piet_runtime.p_input_char()
-    assert np.array_equal(piet_runtime.stack._stack, np.array([32, 116, 101, 115, 116], dtype=np.int64))
-    piet_runtime.stack._stack = np.array([], dtype=np.int64)
+    assert piet_runtime.stack._stack == deque((32, 116, 101, 115, 116))
 
 
-def test_output_num_instruction():
-    global piet_runtime
+def test_output_num_instruction(piet_runtime: PietRuntime):
     num = random.choice(range(1, 100))
     piet_runtime.p_push(num)
     assert piet_runtime.p_output_num() == num
 
 
-def test_output_char_instruction():
-    global piet_runtime
+def test_output_char_instruction(piet_runtime: PietRuntime):
     char = random.choice(string.ascii_letters)
     piet_runtime.p_push(ord(char))
     assert piet_runtime.p_output_char() == char
