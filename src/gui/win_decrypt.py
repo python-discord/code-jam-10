8aa669780d542a1c4c4f350f5800a38c01211b56
Harshit <harsjoshi1614@gmail.com>
2023-09-10 01:38:33 +0530

    Copy to Clipboard feature added

--- src/gui/win_decrypt.py
+++ src/gui/win_decrypt.py
@@ -49,31 +49,48 @@ class DecryptWin(Frame):
             self.mainframe, image=ImageTk.PhotoImage(self.image), bg=DARK_GRAY
         )
         self.key = StringVar()
-        self.key.set(f"Secret Key: {root.key}")
+        self.key.set(f"Secret Key: {root.key} | (Click to Copy)")
+
         self.info = StringVar()
-        self.info.set(f"{len(decoded_text)} characters   |   {self.image.width}px x {self.image.height}px")
+        self.info.set(
+            f"{len(decoded_text)} characters   |   {self.image.width}px x {self.image.height}px"
+        )
         self.canvas.pack(side="left", fill="both", anchor="w")
         self.text.pack(side="right", expand=True, fill="both", anchor="ne")
-        self.mainframe.grid(row=0, column=0, columnspan=2, sticky='nsew')
-        Label(self, textvariable=self.key, bg=DARK_GRAY, fg="white").grid(
-            row=1, column=0, sticky="w"
-        )
+        self.mainframe.grid(row=0, column=0, columnspan=2, sticky="nsew")
+
+        self.key_status = Label(self, textvariable=self.key, bg=DARK_GRAY, fg="white")
+        self.key_status.grid(row=1, column=0, sticky="w")
+
+        self.key_status.bind("<Button-1>", self.copy_key_to_clipboard)
+
         Label(self, textvariable=self.info, bg=DARK_GRAY, fg="white").grid(
             row=1, column=1, sticky="e"
         )
         root.bind("<Configure>", self.updatecanvas)
 
+    def copy_key_to_clipboard(self, _event):
+        """Copies key used to encrypt images to the clipboard"""
+        # Copying to clipboard
+        self.clipboard_clear()
+        self.clipboard_append(self.root.key)
+        # Updating UI
+        self.key.set(f"Secret Key: {self.root.key} | (Copied)")
+        self.root.after(
+            2000, lambda: self.key.set(f"Secret Key: {self.root.key} | (Click to Copy)")
+        )
+
     def updatecanvas(self, event=None):
         """Updates the canvas to fill the screen"""
         if self.mode == "Typing Colors":
             sf = max(1, (self.grid_bbox(0, 0)[3]) // self.ar_height)
-            w, h = int(self.ar_width*sf), int(self.ar_height*sf)
+            w, h = int(self.ar_width * sf), int(self.ar_height * sf)
         else:
             if self.aspect_ratio < 1:  # tall image, use width
                 sf = max(1, self.grid_bbox(0, 0)[2] // self.image.width)
             else:  # opposite
                 sf = max(1, self.grid_bbox(0, 0)[3] // self.image.height)
-            w, h = int(self.image.width*sf), int(self.image.height*sf)
+            w, h = int(self.image.width * sf), int(self.image.height * sf)
         img = ImageTk.PhotoImage(self.image.resize((w, h), BOX))
         self.canvas.configure(image=img, width=w, height=h)
         self.canvas.image = img
@@ -86,7 +103,7 @@ class DecryptWin(Frame):
         """Exports the decrypted text to a file"""
         filename = fd.asksaveasfilename(title="Export As", filetypes=[("TXT", "*.txt")])
         if filename:
-            open(filename, 'w').write(self.text.get(1.0, 'end'))
+            open(filename, "w").write(self.text.get(1.0, "end"))
 
     def edit_key(self):
         """Edits decryption key"""
