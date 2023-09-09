140c454573dd7886a6954f0ed9e752f91355641e
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-09-09 15:28:28 -0700

    Rename piet_runtime ->runtime

--- src/tests/test_runtime.py
+++ src/tests/test_runtime.py
@@ -7,144 +7,144 @@ import pytest
 from src.piet.runtime import CodelChooserDirection, PietRuntime, PietStack, PointerDirection
 
 
-@pytest.fixture(name="piet_runtime")
-def fixture_piet_runtime():
+@pytest.fixture(name="runtime")
+def fixture_runtime():
     return PietRuntime(output_buffer=StringIO())
 
 
-def test_push_instruction(piet_runtime: PietRuntime):
-    piet_runtime.p_push(1)
-    assert piet_runtime.stack[0] == 1
+def test_push_instruction(runtime: PietRuntime):
+    runtime.p_push(1)
+    assert runtime.stack[0] == 1
 
 
-def test_pop_instruction(piet_runtime: PietRuntime):
-    piet_runtime.p_push(1)
-    piet_runtime.p_pop()
-    assert len(piet_runtime.stack) == 0
+def test_pop_instruction(runtime: PietRuntime):
+    runtime.p_push(1)
+    runtime.p_pop()
+    assert len(runtime.stack) == 0
 
 
-def test_add_instruction(piet_runtime: PietRuntime):
+def test_add_instruction(runtime: PietRuntime):
     num1, num2 = random.choice(range(1, 100)), random.choice(range(1, 100))
-    piet_runtime.p_push(num1)
-    piet_runtime.p_push(num2)
-    piet_runtime.p_add()
-    assert piet_runtime.stack[0] == num1 + num2
-    piet_runtime.p_pop()
+    runtime.p_push(num1)
+    runtime.p_push(num2)
+    runtime.p_add()
+    assert runtime.stack[0] == num1 + num2
+    runtime.p_pop()
 
 
-def test_subtract_instruction(piet_runtime: PietRuntime):
+def test_subtract_instruction(runtime: PietRuntime):
     num1, num2 = random.choice(range(1, 100)), random.choice(range(1, 100))
-    piet_runtime.p_push(num1)
-    piet_runtime.p_push(num2)
-    piet_runtime.p_subtract()
-    assert piet_runtime.stack[0] == num1 - num2
-    piet_runtime.p_pop()
+    runtime.p_push(num1)
+    runtime.p_push(num2)
+    runtime.p_subtract()
+    assert runtime.stack[0] == num1 - num2
+    runtime.p_pop()
 
 
-def test_multiply_instruction(piet_runtime: PietRuntime):
+def test_multiply_instruction(runtime: PietRuntime):
     num1, num2 = random.choice(range(1, 100)), random.choice(range(1, 100))
-    piet_runtime.p_push(num1)
-    piet_runtime.p_push(num2)
-    piet_runtime.p_multiply()
-    assert piet_runtime.stack[0] == num1 * num2
-    piet_runtime.p_pop()
+    runtime.p_push(num1)
+    runtime.p_push(num2)
+    runtime.p_multiply()
+    assert runtime.stack[0] == num1 * num2
+    runtime.p_pop()
 
 
-def test_division_instruction(piet_runtime: PietRuntime):
+def test_division_instruction(runtime: PietRuntime):
     num1, num2 = random.choice(range(1, 100)), random.choice(range(1, 100))
-    piet_runtime.p_push(num1)
-    piet_runtime.p_push(num2)
-    piet_runtime.p_divide()
-    assert piet_runtime.stack[0] == num1 // num2
-    piet_runtime.p_pop()
+    runtime.p_push(num1)
+    runtime.p_push(num2)
+    runtime.p_divide()
+    assert runtime.stack[0] == num1 // num2
+    runtime.p_pop()
 
 
-def test_modulo_instruction(piet_runtime: PietRuntime):
+def test_modulo_instruction(runtime: PietRuntime):
     num1, num2 = random.choice(range(1, 100)), random.choice(range(1, 100))
-    piet_runtime.p_push(num1)
-    piet_runtime.p_push(num2)
-    piet_runtime.p_modulo()
-    assert piet_runtime.stack[0] == num1 % num2
-    piet_runtime.p_pop()
+    runtime.p_push(num1)
+    runtime.p_push(num2)
+    runtime.p_modulo()
+    assert runtime.stack[0] == num1 % num2
+    runtime.p_pop()
 
 
-def test_not_instruction(piet_runtime: PietRuntime):
+def test_not_instruction(runtime: PietRuntime):
     choice = random.choice([1, 0])
-    piet_runtime.p_push(choice)
-    piet_runtime.p_not()
-    assert piet_runtime.stack[0] == (not choice)
-    piet_runtime.p_pop()
+    runtime.p_push(choice)
+    runtime.p_not()
+    assert runtime.stack[0] == (not choice)
+    runtime.p_pop()
 
 
-def test_greater_instruction(piet_runtime: PietRuntime):
+def test_greater_instruction(runtime: PietRuntime):
     num1, num2 = random.choice(range(1, 100)), random.choice(range(1, 100))
-    piet_runtime.p_push(num1)
-    piet_runtime.p_push(num2)
-    piet_runtime.p_greater()
-    assert piet_runtime.stack[0] == (num1 > num2)
-    piet_runtime.p_pop()
+    runtime.p_push(num1)
+    runtime.p_push(num2)
+    runtime.p_greater()
+    assert runtime.stack[0] == (num1 > num2)
+    runtime.p_pop()
 
 
-def test_pointer_instruction(piet_runtime: PietRuntime):
-    piet_runtime.p_push(2)
-    piet_runtime.p_pointer()
-    assert piet_runtime.pointer.direction == PointerDirection.LEFT
+def test_pointer_instruction(runtime: PietRuntime):
+    runtime.p_push(2)
+    runtime.p_pointer()
+    assert runtime.pointer.direction == PointerDirection.LEFT
 
 
-def test_switch_instruction(piet_runtime: PietRuntime):
-    piet_runtime.p_push(1)
-    piet_runtime.p_switch()
-    assert piet_runtime.codel_chooser.direction == CodelChooserDirection.RIGHT
+def test_switch_instruction(runtime: PietRuntime):
+    runtime.p_push(1)
+    runtime.p_switch()
+    assert runtime.codel_chooser.direction == CodelChooserDirection.RIGHT
 
 
-def test_duplicate_instruction(piet_runtime: PietRuntime):
+def test_duplicate_instruction(runtime: PietRuntime):
     num = random.choice(range(1, 100))
-    piet_runtime.p_push(num)
-    piet_runtime.p_duplicate()
-    assert piet_runtime.stack.pop_multiple() == [num, num]
+    runtime.p_push(num)
+    runtime.p_duplicate()
+    assert runtime.stack.pop_multiple() == [num, num]
 
 
-def test_roll_instruction(piet_runtime: PietRuntime):
+def test_roll_instruction(runtime: PietRuntime):
     base_stack = PietStack(random.randbytes(random.choice(range(1, 10))))
     edge_stack = PietStack(random.randbytes(random.choice(range(1, 10))))
     roll_count = random.choice(range(1, 10))
-    piet_runtime.stack.extend(base_stack)
-    piet_runtime.stack.extend(edge_stack)
-    piet_runtime.stack.extend((len(edge_stack), roll_count))
+    runtime.stack.extend(base_stack)
+    runtime.stack.extend(edge_stack)
+    runtime.stack.extend((len(edge_stack), roll_count))
     edge_stack.rotate(roll_count)
-    piet_runtime.p_roll()
+    runtime.p_roll()
     base_stack.extend(edge_stack)
-    assert piet_runtime.stack == base_stack
+    assert runtime.stack == base_stack
 
 
-def test_input_num_instruction(piet_runtime: PietRuntime):
-    piet_runtime.input = StringIO("69 test")
-    piet_runtime.p_input_num()
-    result = piet_runtime.input.read()
-    assert piet_runtime.stack.top == 69
+def test_input_num_instruction(runtime: PietRuntime):
+    runtime.input = StringIO("69 test")
+    runtime.p_input_num()
+    result = runtime.input.read()
+    assert runtime.stack.top == 69
     assert result == " test"
 
 
-def test_input_char_instruction(piet_runtime: PietRuntime):
-    piet_runtime.input = StringIO("test")
-    piet_runtime.p_input_char()
-    piet_runtime.p_input_char()
-    piet_runtime.p_input_char()
-    piet_runtime.p_input_char()
-    assert piet_runtime.stack == PietStack((116, 101, 115, 116))
+def test_input_char_instruction(runtime: PietRuntime):
+    runtime.input = StringIO("test")
+    runtime.p_input_char()
+    runtime.p_input_char()
+    runtime.p_input_char()
+    runtime.p_input_char()
+    assert runtime.stack == PietStack((116, 101, 115, 116))
 
 
-def test_output_num_instruction(piet_runtime: PietRuntime):
+def test_output_num_instruction(runtime: PietRuntime):
     num = random.choice(range(1, 100))
-    piet_runtime.p_push(num)
-    piet_runtime.p_output_num()
-    piet_runtime.output.seek(0)
-    assert piet_runtime.output.read() == str(num)
+    runtime.p_push(num)
+    runtime.p_output_num()
+    runtime.output.seek(0)
+    assert runtime.output.read() == str(num)
 
 
-def test_output_char_instruction(piet_runtime: PietRuntime):
+def test_output_char_instruction(runtime: PietRuntime):
     char = random.choice(string.ascii_letters)
-    piet_runtime.p_push(ord(char))
-    piet_runtime.p_output_char()
-    piet_runtime.output.seek(0)
-    assert piet_runtime.output.read() == char
+    runtime.p_push(ord(char))
+    runtime.p_output_char()
+    runtime.output.seek(0)
+    assert runtime.output.read() == char
