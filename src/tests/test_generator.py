a307b1b3ee65e5ea1ff51ba7dd9dcfe6928863ff
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-09-09 21:46:42 -0700

    Add generator key test

--- src/tests/test_generator.py
+++ src/tests/test_generator.py
@@ -21,3 +21,23 @@ def test_generator(data: bytes, cols: int):
         raise exc
     result = interpreter.output
     assert result == data, f"Output does not match input.\n{result}"
+
+
+@pytest.mark.parametrize(
+    ("data", "cols", "key"),
+    [
+        (b"AB", 1, b"passkey"),
+        (b"Hello, world! This a test string.", 4, b"a longer passkey"),
+    ],
+)
+def test_generator_with_key(data: bytes, cols: int, key: bytes):
+    key *= len(data) // len(key) + 1
+    generator = ImageGenerator()
+    encoded = generator.generate_image(data, cols, key)
+    encoded.save(f"{__file__}.png")
+    interpreter = PietInterpreter(encoded, input=key, debug=True)
+    exc = interpreter.run()
+    if isinstance(exc, StepLimitReached):
+        raise exc
+    result = interpreter.output
+    assert result == data, f"Output does not match input.\n{result}"
