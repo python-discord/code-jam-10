46541f879738eede7d2669bfba667eb1329f974e
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-09-09 22:15:55 -0700

    Add some log prints to the cli

--- src/piet/cli.py
+++ src/piet/cli.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 from typing import Annotated, Optional
 
@@ -19,15 +20,24 @@ def generate(
     step_limit: int = 1_000_000,
     debug: bool = False,
 ):
+    print("Generating a Piet program...")
+    print("This process may take a while, depending on the size of the data.")
     if input_path.exists():
         data = input_path.read_bytes()
     else:
         data = str(input_path).encode()
+    command = f'python -m {__package__} run "{output_path}"'
     generator = ImageGenerator(input=input, step_limit=step_limit, debug=debug)
     key = input.encode()
-    key *= len(data) // len(key) + 1
+    if key:
+        key *= len(data) // len(key) + 1
+        command += f' --input "{key.decode()}"'
     image = generator.generate_image(data, cols, key)
     image.save(output_path)
+    print("\n")
+    print("Successfully generated a Piet program!")
+    print(f"Image saved to: {output_path}")
+    print(f"You can run it with: {command}")
 
 
 @app.command()
@@ -45,10 +55,12 @@ def run(
         step_limit=step_limit,
         debug=debug,
     )
-    if output_path:
-        interpreter.runtime.output = output_path.open("wb")
+    interpreter.runtime.output = (
+        output_path.open("wb") if output_path else open(sys.stdout.fileno(), "wb", closefd=False)
+    )
     interpreter.run()
-    print(interpreter.output.decode())
+    print("\n")
+    print("Successfully executed the Piet program!")
 
 
 if __name__ == "__main__":
