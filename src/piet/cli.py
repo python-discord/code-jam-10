c03ecda7ea57d19d39ca50f68fc3b284d6aff0b9
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-09-09 21:28:04 -0700

    Update cli, add shifting with key

--- src/piet/cli.py
+++ src/piet/cli.py
@@ -6,7 +6,6 @@ from PIL import Image
 
 from .generator import ImageGenerator
 from .interpreter import PietInterpreter
-from .runtime import PietRuntime
 
 app = typer.Typer()
 
@@ -16,13 +15,18 @@ def generate(
     input_path: Path,
     output_path: Path,
     cols: int = 2,
+    input: str = "",
+    step_limit: int = 1_000_000,
+    debug: bool = False,
 ):
     if input_path.exists():
         data = input_path.read_bytes()
     else:
         data = str(input_path).encode()
-    generator = ImageGenerator()
-    image = generator.generate_image(data, cols)
+    generator = ImageGenerator(input=input, step_limit=step_limit, debug=debug)
+    key = input.encode()
+    key *= len(data) // len(key) + 1
+    image = generator.generate_image(data, cols, key)
     image.save(output_path)
 
 
@@ -35,18 +39,16 @@ def run(
     debug: bool = False,
 ):
     image = Image.open(image_path).convert("RGB")
-    extra_args = {}
-    if output_path:
-        output_buffer = output_path.open("wb")
-        extra_args["runtime"] = PietRuntime(output_buffer=output_buffer)
     interpreter = PietInterpreter(
         image,
         input=input,
         step_limit=step_limit,
         debug=debug,
-        **extra_args,
     )
+    if output_path:
+        interpreter.runtime.output = output_path.open("wb")
     interpreter.run()
+    print(interpreter.output.decode())
 
 
 if __name__ == "__main__":
