89c78767ec25d00300ac48531f7a276e160a052c
Yubo Wang <yubowang2007@gmail.com>
2023-09-10 11:26:21 +0100

    Works with regex now

--- app/obfuscate/find_boxes.py
+++ app/obfuscate/find_boxes.py
@@ -42,17 +42,30 @@ def find_bounds(image_file: str, text: str, regex: bool = False) -> list[tuple[i
             for paragraph in block.paragraphs:
                 for word in paragraph.words:
                     for symbol in word.symbols:
-                        image_chars.append(
-                            [
-                                symbol.text.lower(),
-                                (
-                                    symbol.bounding_box.vertices[0].x,
-                                    symbol.bounding_box.vertices[0].y,
-                                    symbol.bounding_box.vertices[2].x,
-                                    symbol.bounding_box.vertices[2].y
-                                )
-                            ]
-                        )
+                        if not regex:
+                            image_chars.append(
+                                [
+                                    symbol.text.lower(),
+                                    (
+                                        symbol.bounding_box.vertices[0].x,
+                                        symbol.bounding_box.vertices[0].y,
+                                        symbol.bounding_box.vertices[2].x,
+                                        symbol.bounding_box.vertices[2].y
+                                    )
+                                ]
+                            )
+                        else:
+                            image_chars.append(
+                                [
+                                    symbol.text,
+                                    (
+                                        symbol.bounding_box.vertices[0].x,
+                                        symbol.bounding_box.vertices[0].y,
+                                        symbol.bounding_box.vertices[2].x,
+                                        symbol.bounding_box.vertices[2].y
+                                    )
+                                ]
+                            )
 
     if not regex:
         text_index = 0
@@ -75,6 +88,28 @@ def find_bounds(image_file: str, text: str, regex: bool = False) -> list[tuple[i
             elif text[text_index].lower() != image_char[0]:
                 text_index = 0
                 temp_bounds.clear()
+    else:
+        for regex_text in text:
+            text_index = 0
+            temp_bounds = []
+            for image_char in image_chars:
+                try:
+                    while regex_text[text_index] == " ":
+                        text_index += 1
+                except IndexError as e:
+                    print(e)
+                    text_index = 0
+                    temp_bounds.clear()
+                if regex_text[text_index] == image_char[0]:
+                    text_index += 1
+                    temp_bounds.append(image_char[1])
+                    if text_index == len(regex_text):
+                        bounds.extend(temp_bounds.copy())
+                        temp_bounds.clear()
+                        text_index = 0
+                elif regex_text[text_index] != image_char[0]:
+                    text_index = 0
+                    temp_bounds.clear()
     return bounds
 
 
@@ -85,7 +120,7 @@ if __name__ == "__main__":
 
     from .colour_box import ColourBox
     current_dir = str(pathlib.Path(__file__).parent)
-    bounds = find_bounds(current_dir + '/img2.webp', "Oscar Wilde")
+    bounds = find_bounds(current_dir + '/img2.webp', r"NO ([A-Z]+)", True)
     print(bounds)
     img = Image.open(current_dir + '/img2.webp')
     colour_box = ColourBox((0, 0, 0))
